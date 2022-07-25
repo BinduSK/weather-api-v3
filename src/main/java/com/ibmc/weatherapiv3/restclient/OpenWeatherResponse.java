f513018d655e5f252aa21b293455a1d70d7371b0
BinduSK <bindusajithkumar01@gmail.com>
2022-07-25 17:56:13 +0530

    restclientupdates

---
+++ src/main/java/com/ibmc/weatherapiv3/restclient/OpenWeatherResponse.java
@@ -0,0 +1,13 @@
+package com.ibmc.weatherapiv3.restclient;
+
+public class OpenWeatherResponse {
+    Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
