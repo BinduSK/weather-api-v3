0a2c77d54efdf02ab74252967482bc0436ded099
BinduSK <bindusajithkumar01@gmail.com>
2022-07-25 06:46:54 +0530

    domainupdate

---
+++ src/main/java/com/ibmc/weatherapiv3/domain/Weather.java
@@ -0,0 +1,38 @@
+package com.ibmc.weatherapiv3.domain;
+
+public class Weather {
+    String city,state,country;
+    Double currentTemperature;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Double getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(Double currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+}
